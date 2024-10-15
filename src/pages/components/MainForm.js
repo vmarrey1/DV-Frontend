f4782eb22c3d2a016a1cee3b28e449755a706908
Varun Marrey <varunmarrey@gmail.com>
2024-10-14 19:14:41 -0700

    Changed backend calls
    configured backend call to new backend on AWS server.

--- src/pages/components/MainForm.js
+++ src/pages/components/MainForm.js
@@ -27,7 +27,7 @@ function EnterFile() {
 
     try {
      
-      const response = await axios.post('http://127.0.0.1:5000/upload', formData, {
+      const response = await axios.post('http://54.219.131.177/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
