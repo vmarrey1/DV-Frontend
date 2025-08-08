3ae7064a4722784e8aaab5db56838e6f75303168
varun <varun_marrey@berkeley.edu>
2025-08-07 20:54:24 -0700

    Add static export configuration for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Removed deprecated appDir option
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
